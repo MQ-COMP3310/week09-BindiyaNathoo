8a8511f4c8e6993ce4558709d3a424a80665183e
Bindiya Nathoo <73746483+BindiyaNathoo@users.noreply.github.com>
2025-05-06 18:28:18 +1000

    fixed flask query to run in debug mode

--- run.py
+++ run.py
@@ -2,5 +2,7 @@ from project import create_app
 
 if __name__ == '__main__':
   app = create_app()
+  debug_mode = 
+  os.getenv('FLASK_ENV') == 'development'
   app.run(host = '127.0.0.1', port = 8001, debug=True) 
 
